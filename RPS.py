244679c32541623030d25a3f3ba9bec38e573da3
PaRaZzzziT <daniil.osipov@student.ie.edu>
2018-11-16 09:24:00 +0100

    created RPS function

---
+++ RPS.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 16 08:48:10 2018
+
+@author: Grani
+"""
+
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+
+
+def rock_paper_scissors(player1, player2):
+    if player1 == "rock" and player2 == "paper":
+        return "Player 2 won"
+    elif player1 == "paper" and player2 == "rock":
+        return "Player 1 won"
+    elif player1 == "rock" and player2 == "scissors":
+        return "Player 1 won"
+    elif player1 == "scissors" and player2 == "rock":
+        return "Player 2 won"
+    elif player1 == "scissors" and player2 == "paper":
+        return "Player 1 won"
+    elif player1 == "paper" and player2 == "scissors":
+        return "Player 2 won"
+    elif player1 == player2:
+        return "Draw"
+    else:
+        return None

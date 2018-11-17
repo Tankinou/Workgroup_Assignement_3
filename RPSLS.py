6b5b564feae34f95b171df1ff2bdfaf8e6798d1a
Florian Diegruber <florian@diegruber.net>
2018-11-17 13:41:28 +0100

    Added alternative solution that doesn't hurt my eyes :)

--- RPSLS.py
+++ RPSLS.py
@@ -4,7 +4,7 @@ Created on Fri Nov 16 18:48:22 2018
 
 @author: Grani
 """
-
+#%%
 rock = "rock"
 paper = "paper"
 scissors = "scissors"
@@ -57,3 +57,22 @@ def rock_paper_scissors_lizard_spock(player1, player2):
         return "Player 1 won"
     else:
         return None
+#%%
+        
+#Alternative solution :)    
+        
+    
+def rock_paper_scissors_spock_inCool(player1,player2):
+    key = {"rock":0,"spock":1,"paper":2,"lizard":3,"scissors":4}
+    n1 = key[player1]
+    n2 = key[player2]
+    dif = (n1 - n2)%5
+    if dif == 0:
+        return "Draw"
+    elif dif == 1 or dif == 2:
+        return "Player 1 won"
+    elif dif == 3 or dif == 4:
+        return "Player 2 won"
+    else:
+        return None
+#%%    

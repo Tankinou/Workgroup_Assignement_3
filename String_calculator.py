4364d85582985ad6c3d148b2bc92fb2544af1a58
Florian Diegruber <florian@diegruber.net>
2018-11-19 13:10:51 +0100

    Update String_Calculator

--- String_calculator.py
+++ String_calculator.py
@@ -7,24 +7,29 @@ Created on Fri Nov 16 09:14:23 2018
 """
 #%%
 
+def isnumber(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
 def string_calculator(myString):
-	if myString == "":
-		return 0;
-	myList = myString.split(',')
-	sum = 0
-	for number in myList:
-		sum = sum + (int)(number)
-	return sum;
+    if myString == "":
+        return 0
+    else:
+        myList = myString.split(',')
+        counter = 0
+        for i in myList:
+            if isnumber(i) == False:
+                return "Input string not correct"
+        else:
+        	for number in myList:
+        		counter += int(number)
+    return counter
 
-
-def main():
-	myString = (str)(raw_input("Enter the desired string: "))
-	print(string_calculator(myString))
-
-main()
 
 #%%
-
 
 
      
